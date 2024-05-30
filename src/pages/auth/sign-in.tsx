ee79ecfa7e1331cd29c360de562e9422ed3b4a68
GadiegoN <ngadiego@gmail.com>
2024-05-30 17:57:18 -0300

    .

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { auth } from "@/services/firebase-connection"
 import { signInWithEmailAndPassword } from "firebase/auth"
